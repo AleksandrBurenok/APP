2190a872df3e1e1305b4dfc13f37c813d5088244
Aleksandr <oleksandrburenok@gmail.com>
2019-02-22 18:23:44 +0200

    add new lessons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import App from './events/Lesson.jsx';
+import Table from './fragment_css/Lesson.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Table />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
